fbb80dd8b8f0acad0bc9e1ea6b0ff6489cec64c6
Lolfoollor <L_Z_F47@hotmail.com>
2025-04-27 22:17:23 +0800

    style: add theme style and add json recognition

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,3 +2,8 @@ declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
