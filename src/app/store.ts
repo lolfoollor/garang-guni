689d6430bf85b410d70ee942796e240fd92194ba
Lolfoollor <L_Z_F47@hotmail.com>
2025-06-20 17:53:02 +0800

    feat(app): add Redux store with persist and image IndexedDB support
    - Set up Redux store and integrate Redux Persist for state persistence
    - Initialize IndexedDB logic for image storage

---
+++ src/app/store.ts
@@ -0,0 +1,56 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import bookingDraftReducer, {
+  setDraft,
+} from "@/features/bookings/bookingDraftSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import { loadImageBlob } from "./db";
+import { Image } from "@/components/Image";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["bookingDraft"],
+};
+
+const rootReducer = combineReducers({
+  bookingDraft: bookingDraftReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+const refreshImageBlobs = async () => {
+  const images = store.getState().bookingDraft.images;
+  if (!images || images.length <= 0) {
+    return;
+  }
+
+  const updatedImgs = await Promise.all(
+    images.map(async (image: Image) => {
+      const updatedImgBlob = await loadImageBlob(image.id);
+      return { id: image.id, src: updatedImgBlob };
+    }),
+  );
+
+  store.dispatch(setDraft({ images: updatedImgs }));
+};
+
+export const persistor = persistStore(store);
+persistor.subscribe(async () => {
+  const { bootstrapped } = persistor.getState();
+  if (bootstrapped) {
+    await refreshImageBlobs();
+  }
+});
+
+export type AppStore = typeof store;
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "./app/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import Loading from "./components/Loading.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 );
