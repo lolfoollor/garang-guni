00be7c14715d99630806ee3e2aeb9639f64b3982
Lolfoollor <L_Z_F47@hotmail.com>
2025-07-16 01:01:59 +0800

    refactor(layout): wrap outlet in RootLayout with Suspense for lazy loading

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,14 +2,26 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import styles from "./RootLayout.module.css";
+import Loading from "@/components/Loading";
+import { Suspense } from "react";
+import NavBar from "@/components/NavBar";
 
 function RootLayout() {
+  const LoadingPage = (
+    <div className="flex min-h-screen items-center justify-center">
+      <Loading />
+    </div>
+  );
+
   return (
     <main className={styles.mainContainer}>
       <Header />
-      <div className={styles.outletContainer}>
-        <Outlet />
-      </div>
+      <NavBar />
+      <Suspense fallback={LoadingPage}>
+        <div className={styles.outletContainer}>
+          <Outlet />
+        </div>
+      </Suspense>
       <Footer />
     </main>
   );
